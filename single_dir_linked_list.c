100be045f48173654f03b7c331b4518340bb2a1d
komarcin11 <99415909+komarcin11@users.noreply.github.com>
2024-01-20 12:22:46 +0100

    Add files via upload
    finall touches need additional cs50.c lib to work but its finished

--- single_dir_linked_list.c
+++ single_dir_linked_list.c
@@ -8,153 +8,132 @@ typedef struct node{//remeber to add name of stuct as well as at the end
     struct node* next;
 } node;
 
-void visualize( node* position);
+void visualize(node* position);
+node* add_first(node* list);
+node* append(node* list);
+void free_memo(node* list);
+node* decide(char x, node* list);
 
 int main(void){
     node* list = NULL;
-    //inserting first element
-    string word;
-    node *n;
-    //___________________________________________________
 
-    //add first
-    word = get_string("Enter your list element: ");
-    n = malloc(sizeof(node));
-    if(n == NULL){
-        printf("no memory\n");
-        free(n);
-        return 1;
+    while (true){ // saking user what to do 
+        printf("what you want to do:\n");
+        char c;
+        if (list == NULL){
+            printf("add first element? 'f'\n");
+            printf("quit ? 'q'\n");
+            c = get_char("insert here your anwser: ");
+            if (c=='f'){
+                list = decide(c,list); // passing the imput to the function
+                continue;
+            }
+            else if (c=='q'){
+                printf("Exiting programs\n");
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+        else if(list !=NULL){
+            printf("append to the list? 'a'\n");
+            printf("add as first? 'f' \n");
+            // printf("insert inside? 'i'\n");
+            // printf("delete an element? 'd'\n");
+            printf("quit ? 'q'\n");
+            c = get_char("insert here your anwser: ");
+            if (c=='f'||c=='a'){
+                list = decide(c,list);
+                continue;
+            }
+            else if (c=='q'){
+                printf("Exiting programs\n");
+                break;
+            }
+            else{
+                continue;
+            }
+        }
     }
-    n->content = word;
-    n->next = NULL;
-    n->next = list;
-    list = n;
-    //___________________________________________________
-
-    //add at the head od the list
-    word = get_string("Enter your list element: ");
-    n = malloc(sizeof(node));
-    if(n == NULL){
-        printf("no memory\n");
-        free(n);
-        return 1;
-    }
-    n->content = word;
-    n->next = NULL;
-    n->next = list;
-    list = n;
-    //___________________________________________________
-
-    //append TODO
-    word = get_string("Enter your list element: ");
-    n = malloc(sizeof(node));
-    if(n == NULL){
-        printf("no memory\n");
-        free(n);
-        return 1;
-    }
-    n->content = word;
-    n->next = NULL;
-    n->next = list;
-    list = n;
-
-    node* ptr = list;
-    int i = 0;
-    while(i!=3){
-        printf("ppointer position: %p\n",ptr);
-        ptr = list->next;//correct
-        
-        
-        i++;
-    }//---------------- that is not dooing good
 
     visualize(list);
-    free(list);
-    // free(n);
-
+    free_memo(list);
+    return 0;
 }
 
 void visualize(node* position){
-    printf("\n+--List Visualise--+\n\n");
+    printf("\n+----List Visualise----+\n\n");
     while(position != NULL){
         printf("Location:%p\n", position);
         printf("Phrase:\"%s\"\n", position->content);
         printf("Next: %p\n\n", position->next);
         position = position->next;
     }
-    printf("\n+-----------------+\n\n");
+    printf("\n+---------------------+\n\n");
 }
 
+node* add_first(node* list){
+    string word;
+    node *n;
+    word = get_string("Enter your list element: ");
+    n = malloc(sizeof(node));
+    if(n == NULL){
+        printf("no memory\n");
+        free(n);
+    }
+    n->content = word;
+    n->next = NULL;
+    n->next = list;
+    list = n;
+    return list;
+}
 
+node* append(node* list){
+    string word;
+    node *n;
+    node* tmp;
+    tmp = list;
+    while(tmp->next!=NULL){
+        tmp = tmp->next;
+    }
+    word = get_string("Enter your list element: ");
+    n = malloc(sizeof(node));
+    if(n == NULL){
+        printf("no memory\n");
+        free(n);
+    }
+    n->content = word;
+    n->next = NULL;
+    tmp->next = n;
+    return list;
+}
 
-// void decide(char x, node* list){
-//     if ( x=='q'){
-//         printf("exiting program\n");
-//         free_memo();
-//         exit(0);
-//     }
-//     else if(x=='f'){
-//         //fun element a the head of l. list
-//     }
-//     else if(x=='a'){
-//         //fun element that appends element
-//     }
-//     else if(x=='i'){
-//         //fun element that inserts element
-//     }
-//     else if(x=='i'){
-//         //fun element that inserts element
-//     }
-//     else if(x=='q'){
-//         //fun element that inserts element
-//     }
-//     else if(x=='d'){
-//         //fun to delete elements
-//     }
-// }
+void free_memo(node* list){
+    while(list != NULL){
+        node* tmp = list->next;
+        free(list);
+        list = tmp;
+        
+    }
+    printf("memory is freed\n\n");
+}
 
-// void free_memo(void){
-//     //memory freeing
-// }
-
-// void add_at_beginig(string str){
-//     //add at the begining if no elements or not
-// }
-
-// void append(string str){
-//     //add element a te the end
-// }
-
-
-
-
-
-
-    // while (true){
-    //     printf("what you want to do:");
-    //     char c;
-    //     if (list == NULL){
-    //         printf("add first element? 'f'\n");
-    //         printf("quit ? 'q'\n");
-    //         c = get_char("insert here your anwser: ");
-    //         if (c=='q' || c=='f'){
-    //             decide(c,list);
-    //         }
-    //         else{
-    //             continue;
-    //         }
-    //     }
-    //     else if(list !=NULL){
-    //         printf("append to the list? 'a'\n");
-    //         printf("add as first? 'f' \n");
-    //         printf("insert inside? 'i'\n");
-    //         printf("delete an element? 'd'\n");
-    //         printf("quit ? 'q'\n");
-    //         c = get_char("insert here your anwser: ");
-    //         decide(c,list);
-    //         continue;
-    //     }
-    // }
-
+node* decide(char x, node* list){
+    if (x=='f'){
+        list=add_first(list);
+        visualize(list);
+        return list;
+    }
+    else if(x=='a'){
+        list = append(list);
+        visualize(list);
+        return list;
+    }
+    else{
+        return list;
+    }
+    printf("\n-------------------------\n");
+}
 
 
